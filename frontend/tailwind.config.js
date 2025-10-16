3801b3351e9c2b7d99d72bb80ad873c26a1c1d97
Karthik Raj <karthik@example.com>
2025-10-16 16:02:00 +0530

    Updated

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,5 +1,15 @@
-/** @type {import('tailwindcss').Config} */
-export default {
+// /** @type {import('tailwindcss').Config} */
+// export default {
+//   darkMode: "class", 
+//   content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// };
+
+
+module.exports = {
   darkMode: "class",
   content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
